4bda66ac1da4b79e9da49225e5f86b80cd6e90d6
Opeyemi541 <120164089+Opeyemi541@users.noreply.github.com>
2023-02-16 12:40:10 +0400

    lessons.js added

---
+++ lessons.js
@@ -0,0 +1,100 @@
+const lessons = [
+    {   
+        "id":"001",
+        "Subject":"Math",
+        "Location":"London",
+        "Price":"350",
+        "Space": 10
+        ,
+        "image":"Math4-web.jpg",
+        
+    },
+    {   
+        "id":"002",
+        "Subject":"English",
+        "Location":"Dubai",
+        "Price":"360",
+        "Space": 10,
+        "image":"english.jpg",
+        
+
+    },
+    {
+        "id":"003",
+        "Subject":"Computer Science",
+        "Location":"Norway",
+        "Price":"250",
+        "Space":10,
+        "image":"computer science.jpg",
+        
+
+    },
+    {
+        "id":"004",
+        "Subject":"History",
+        "Location":"India",
+        "Price":"200",
+        "Space":10,
+        "image":"history.jpg",
+        
+
+    },
+    {
+        "id":"005",
+        "Subject":"Geography",
+        "Location":"Abu Dahbi",
+        "Price":"300",
+        "Space":10,
+        "image":"geography.jpg",
+        
+    },
+    {
+        "id":"006",
+        "Subject":"Social Studies",
+        "Location":"New York",
+        "Price":"220",
+        "Space":10,
+        "image":"ss.jpg",
+        
+
+    },
+    {
+        "id":"007",
+        "Subject":"Psychology",
+        "Location":"London",
+        "Price":"320",
+        "Space":10,
+        "image":"psy.jpg",
+        
+
+    },{
+        "id":"008",
+        "Subject":"Biology",
+        "Location":"Jumiera",
+        "Price":"170",
+        "Space":10,
+        "image":"bio.jpeg",
+        
+
+    },
+    {
+        "id":"009",
+        "Subject":"Nuclear Physics",
+        "Location":"China",
+        "Price":"500",
+        "Space":10,
+        "image":"NP.jpg",
+        
+    },
+    {
+        "id":"010",
+        "Subject":"Rocket Science",
+        "Location":"Japan",
+        "Price":"510",
+        "Space":10,
+        "image":"rockect sci.jpg",
+        
+
+    }
+];
+module.exports = lessons;
