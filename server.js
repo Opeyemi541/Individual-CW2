08e58119da5d1f042092d58ff8a487641b957131
Opeyemi541 <120164089+Opeyemi541@users.noreply.github.com>
2023-02-16 23:35:01 +0400

    updated server listening

--- server.js
+++ server.js
@@ -83,6 +83,7 @@ app.post("/orders", (req, res) => {
   res.json(searchResults);
 });
   
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
